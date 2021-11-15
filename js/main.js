f81f4aede5d0ad8ceb73f94b22cd1f22fe62ecf4
nabe7855 <94083814+nabe7855@users.noreply.github.com>
2021-11-15 14:06:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,86 @@
-'use strict';
+'use strict'
 
 {
-function callback(entries, obs) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return
-    }
-    entry.target.classList.add('appear');
-    obs.unonserver(entry.target);
-  });
+const open = document.getElementById('open');
+const overlay =document.querySelector('.overlay');
+const close =document.getElementById('close');
+open.addEventListener('click', () => {
+  overlay.classList.add('show');
+  open.classList.add('hide');
+});
+close.addEventListener('click', () => {
+  overlay.classList.remove('show');
+  open.classList.remove('hide');
+});
+
+const next =document.getElementById('next');
+const prev =document.getElementById('prev');
+const ul =document.querySelector('ul')
+const slides = ul.children
+const dots = [];
+let currentIndex =0;
+
+
+function updateButtons() {
+  prev.classList.remove('hidden')
+  next.classList.remove('hidden')
+
+  if (currentIndex === 0) {
+    prev.classList.add('hidden');
+  }
+  if (currentIndex === slides.length - 1) {
+    next.classList.add('hidden');
+  }
 }
-const options = {
-  threshold: 0.2,
+
+function moveSlides() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  ul.style.transform = `translateX(${-1*slideWidth * currentIndex}px)`;
 }
-  const observer = new IntersectionObserver(callback, options);
-  
-  const targets = document.querySelectorAll('.animate');
 
-  targets.forEach(target => {
-    observer.observe(target);
+function setupDots() {
+  for (let i = 0; i < slides.length; i++) {
+const button = document.createElement('button')
+button.addEventListener('click',() => {
+ currentIndex = i;
+ updateDots();
+ updateButtons();
+ moveSlides();
+});
+dots.push(button);
+document.querySelector('nav').appendChild(button);
+
+  }
+  dots[0].classList.add('current2');
+
+}
+
+function updateDots() {
+  dots.forEach(dot => {
+    dot.classList.remove('current2');
   });
+  dots[currentIndex].classList.add('current2');
 
+}
 
+updateButtons();
+setupDots()
+
+next.addEventListener('click',() => {
+  currentIndex++;
+  updateButtons();
+  updateDots();
+ moveSlides();
+});
+
+prev.addEventListener('click',() => {
+  currentIndex--;
+  updateDots();
+  updateButtons();
+moveSlides();
+});
+
+window.addEventListener('resize',() => {
+  moveSlides();
+});
 }
